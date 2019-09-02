75a3475be814c12dfa986cfd5fe212a76e5cd851
xbl <blxie@toughtworks.com>
2019-09-02 15:54:37 +0800

    refactor(example): remove unused file

--- test/example.spec.js
+++
@@ -1,8 +0,0 @@
-import assert from 'assert';
-import { add } from '../src/example';
-
-describe('example', function() {
-  it('如果 a 等于 2 ，b 等于 3，当调用 add()，则结果等于 5', function() {
-    assert.equal(add(2, 3), 5);
-  });
-});
