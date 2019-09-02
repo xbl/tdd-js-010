748d96d05e532d58c5cf63c232b826fc3bd397f2
xbl <blxie@toughtworks.com>
2019-09-02 17:56:25 +0800

    test(parentheses.spec): add some case

--- test/parentheses.spec.js
+++ test/parentheses.spec.js
@@ -29,4 +29,20 @@ describe('Parentheses', function() {
   it('如果 输入字符串为 [({})] ，当调用 Parentheses.execute()，则结果返回 true', () => {
     assert.equal(Parentheses.execute('[({})]'), true);
   });
+
+  it('如果 输入字符串为 {}([]) ，当调用 Parentheses.execute()，则结果返回 true', () => {
+    assert.equal(Parentheses.execute('{}([])'), true);
+  });
+
+  it('如果 输入字符串为 {()}[[{}]] ，当调用 Parentheses.execute()，则结果返回 true', () => {
+    assert.equal(Parentheses.execute('{()}[[{}]]'), true);
+  });
+
+  it('如果 输入字符串为 {{)(}} ，当调用 Parentheses.execute()，则结果返回 false', () => {
+    assert.equal(Parentheses.execute('{{)(}}'), false);
+  });
+
+  it('如果 输入字符串为 ({)} ，当调用 Parentheses.execute()，则结果返回 false', () => {
+    assert.equal(Parentheses.execute('({)}'), false);
+  });
 });
