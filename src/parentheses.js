bb390297e16a4af7901cefade805afeec4b0b5e8
xbl <blxie@toughtworks.com>
2019-09-02 15:55:39 +0800

    feat(parentheses): when input str is empty

---
+++ src/parentheses.js
@@ -0,0 +1,8 @@
+
+export default {
+  execute(str) {
+    if (str === '') {
+      return true;
+    }
+  }
+};
---
+++ test/parentheses.spec.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import Parentheses from '../src/parentheses';
+
+describe('Parentheses', function() {
+  it('如果 输入字符串为 "" ，当调用 Parentheses.execute()，则结果返回 true', () => {
+    assert.equal(Parentheses.execute(''), true);
+  });
+});
